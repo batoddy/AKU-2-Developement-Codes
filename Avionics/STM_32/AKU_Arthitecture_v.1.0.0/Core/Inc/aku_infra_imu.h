c32f42e1670e9dbd4b0261945735ff75b20ccad9
Batuhan Odçıkın <batuhan.odcikin@std.yildiz.edu.tr>
2024-02-08 15:47:46 +0300

    imu N barometer codes

---
+++ Avionics/STM_32/AKU_Arthitecture_v.1.0.0/Core/Inc/aku_infra_imu.h
@@ -0,0 +1,20 @@
+/*
+ * aku_infra_imu.h
+ *
+ *  Created on: Jan 30, 2024
+ *      Author: Batuhan
+ */
+
+#ifndef INC_AKU_INFRA_IMU_H_
+#define INC_AKU_INFRA_IMU_H_
+
+#include "aku_structure.h"
+
+
+void init_IMU();
+IMU *read_IMU();
+
+
+
+
+#endif /* INC_AKU_INFRA_IMU_H_ */
